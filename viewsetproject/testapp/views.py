98eaa2c07a048c2075a7cacdc3817f01e919c1cf
naresherla <naresherla334@gmail.com>
2024-09-28 10:56:32 +0530

    second cmt

--- viewsetproject/testapp/views.py
+++ viewsetproject/testapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from testapp.models import Employee
 from rest_framework.viewsets import ModelViewSet
 from testapp.serializers import Employeeserializer
+from django.http import HttpResponse
 class viewsetcrudview(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = Employeeserializer
