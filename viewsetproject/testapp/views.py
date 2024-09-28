32fb362fc03828ddc4af0634f5b98c974325a450
naresherla <naresherla334@gmail.com>
2024-09-28 10:33:40 +0530

    fr cmn

---
+++ viewsetproject/testapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from testapp.models import Employee
+from rest_framework.viewsets import ModelViewSet
+from testapp.serializers import Employeeserializer
+
+class viewsetcrudview(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = Employeeserializer
